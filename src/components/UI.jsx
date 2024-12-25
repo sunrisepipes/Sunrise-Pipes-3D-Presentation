105dc783acd780e235b907d46f8e688ccd0bc3c6
khushalGrover <khushal15grover@gmail.com>
2024-12-25 23:55:56 +0530

    logo link

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -44,7 +44,7 @@ export const UI = () => {
       <main className=" pointer-events-none select-none z-10 fixed  inset-0  flex justify-between flex-col">
         <a
           className="pointer-events-auto mt-10 ml-10"
-          href="https://google.com"
+          href="https://sunrisepipes.vercel.app/"
         >
           {/* <img className="w-20" src="/images/LOGO_TEXT.png" /> */}
           <div className="text-white text-2xl">Sunrise Pipes</div>
